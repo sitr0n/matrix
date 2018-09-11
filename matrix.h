74f07b6eb232f4de5e624b4b64cacbc5f896f02f
sitr0n <jonas_kalland@hotmail.no>
2018-09-12 01:33:53 +0200

    saving for later

---
+++ matrix.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Arduino.h"
+
+class Matrix {
+    public:
+        Matrix(int width, int height);
+        void set(int value, int x, int y);
+        int get(int x, int y);
+        void fillWithValue(int value);
+        
+    private:
+        int _width;
+        int _height;
+
+        typedef uint16_t data;
+        struct element {
+          data value = 0;
+          element *next = NULL;
+        }*origo;
+};
