e998ec5c85d52933af6530119602296c64a074b7
sitr0n <jonas_kalland@hotmail.no>
2018-09-12 01:36:32 +0200

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,57 +1,53 @@
 #include "matrix.h"
 
-
 Matrix::Matrix(int width, int height) :
-  _width(width),
-  _height(height)
+	_width(width),
+	_height(height)
 {
-  Serial.begin(115200);
-  origo = new element;
-  element *node = origo;
-  int size = _width*_height;
-  for (int i = 0; i < size; i++) {
-    node->next = new element;
-    node = node->next;
-  }
+	Serial.begin(115200);
+	origo = new element;
+	element *node = origo;
+	int size = _width*_height;
+	for (int i = 0; i < size; i++) {
+		node->next = new element;
+		node = node->next;
+	}
 }
 
 void Matrix::fillWithValue(int value)
 {
-  element *node = origo;
-  (node)->value = (data) value;
-  int size = _width*_height;
-  for (int i = 0; i < size - 1; i++) {
-    node = node->next;
-    (node)->value = (data) value;
-  }
+	element *node = origo;
+	(node)->value = (data) value;
+	int size = _width*_height;
+	for (int i = 0; i < size - 1; i++) {
+		node = node->next;
+		(node)->value = (data) value;
+	}
 }
 
 void Matrix::set(int value, int x, int y)
 {
-  assert(x < _width);
-  assert(y < _height);
+	assert(x < _width);
+	assert(y < _height);
 
-  element *node = origo;
-  
-  int index = y*_width + x;
-  for (int i = 0; i < index; i++) {
-    node = node->next;
-  }
-  node->value = value;
+	element *node = origo;
+	int index = y*_width + x;
+	for (int i = 0; i < index; i++) {
+		node = node->next;
+	}
+	node->value = value;
 }
 
 int Matrix::get(int x, int y)
 {
-  assert(x < _width);
-  assert(y < _height);
-  
-  element *node = origo;
-  int index = y*_width + x;
-  for (int i = 0; i < index; i++) {
-    //Serial.println((node)->value);
-    node = node->next;
-  }
-  int elementValue = (int) node->value;
-  return elementValue;
-}
+	assert(x < _width);
+	assert(y < _height);
 
+	element *node = origo;
+	int index = y*_width + x;
+	for (int i = 0; i < index; i++) {
+		node = node->next;
+	}
+	int elementValue = (int) node->value;
+	return elementValue;
+}
