1b60755103f2d17d4c9b95b5b6793d67ae1181b2
sitr0n <jonas_kalland@hotmail.no>
2018-09-12 01:37:36 +0200

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -2,19 +2,19 @@
 #include "Arduino.h"
 
 class Matrix {
-    public:
-        Matrix(int width, int height);
-        void set(int value, int x, int y);
-        int get(int x, int y);
-        void fillWithValue(int value);
-        
-    private:
-        int _width;
-        int _height;
+	public:
+		Matrix(int width, int height);
+		void set(int value, int x, int y);
+		int get(int x, int y);
+		void fillWithValue(int value);
 
-        typedef uint16_t data;
-        struct element {
-          data value = 0;
-          element *next = NULL;
-        }*origo;
+	private:
+		int _width;
+		int _height;
+
+		typedef uint16_t data;
+		struct element {
+			data value = 0;
+			element *next = NULL;
+		}*origo;
 };
